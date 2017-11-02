6f135649d5f91d406ce1dc7303338608c61da428
marskr <marskr@opoczta.pl>
2017-11-02 18:09:32 +0100

    Initial commit

---
+++ MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+
+namespace BeerProcessingManager
+{
+    [Activity(Label = "BeerProcessingManager", MainLauncher = true)]
+    public class MainActivity : FragmentActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+            var pager = FindViewById<ViewPager>(Resource.Id.pager);
+            var adaptor = new GenericFragmentPagerAdaptor(SupportFragmentManager);
+            adaptor.AddFragmentView((i, v, b) =>
+            {
+                var view = i.Inflate(Resource.Layout.Tab, v, false);
+                var textSample = view.FindViewById<TextView>(Resource.Id.txtText);
+                textSample.Text = "The first page";
+                return view;
+            });
+            adaptor.AddFragmentView((i, v, b) =>
+            {
+                var view = i.Inflate(Resource.Layout.Tab, v, false);
+                var textSample = view.FindViewById<TextView>(Resource.Id.txtText);
+                textSample.Text = "The second page";
+                return view;
+            });
+
+            pager.Adapter = adaptor;
+            pager.SetOnPageChangeListener(new ViewPageListenerForActionBar(ActionBar));
+
+            ActionBar.AddTab(pager.GetViewPageTab(ActionBar, "First Tab"));
+            ActionBar.AddTab(pager.GetViewPageTab(ActionBar, "Second Tab"));
+        }
+    }
+}
+
